b4b43c19d54cef76954fa92bf88640f7d0825d0f
Sonam Giri <104783179+SonamGiri9150@users.noreply.github.com>
2023-09-11 20:34:30 +0530

    All Resources

---
+++ AllResources1/app/src/main/java/com/sonamakes/allresources/AppDev.kt
@@ -0,0 +1,12 @@
+package com.sonamakes.allresources
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AppDev : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_app_dev)
+    }
+
+}
---
+++ AllResources1/app/src/main/java/com/sonamakes/allresources/WebDev.kt
@@ -0,0 +1,11 @@
+package com.sonamakes.allresources
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WebDev : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_web_dev)
+    }
+}
